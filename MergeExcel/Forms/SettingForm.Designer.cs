a8f0e6fe5aefc0bdf7d6976738d90ae44f4f48ef
hongsifan@bis.com.cn <zxcvbnm>
2020-10-20 13:50:39 +0800

    'd'

---
+++ MergeExcel/Forms/SettingForm.Designer.cs
@@ -0,0 +1,276 @@
+﻿namespace JGNet.Common
+{
+    partial class SettingForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(SettingForm));
+            this.BaseButton_Cancel = new JGNet.Common.Components.BaseButton();
+            this.BaseButton_OK = new JGNet.Common.Components.BaseButton();
+            this.skinLabel4 = new CCWin.SkinControl.SkinLabel();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
+            this.dateTimePicker3 = new System.Windows.Forms.DateTimePicker();
+            this.dateTimePicker4 = new System.Windows.Forms.DateTimePicker();
+            this.skinLabel1 = new CCWin.SkinControl.SkinLabel();
+            this.skinLabel2 = new CCWin.SkinControl.SkinLabel();
+            this.skinLabel3 = new CCWin.SkinControl.SkinLabel();
+            this.skinLabel5 = new CCWin.SkinControl.SkinLabel();
+            this.skinLabel6 = new CCWin.SkinControl.SkinLabel();
+            this.dateTimePicker5 = new System.Windows.Forms.DateTimePicker();
+            this.dateTimePicker6 = new System.Windows.Forms.DateTimePicker();
+            this.dateTimePicker7 = new System.Windows.Forms.DateTimePicker();
+            this.dateTimePicker8 = new System.Windows.Forms.DateTimePicker();
+            this.SuspendLayout();
+            // 
+            // BaseButton_Cancel
+            // 
+            this.BaseButton_Cancel.BackColor = System.Drawing.Color.Transparent;
+            this.BaseButton_Cancel.ControlState = CCWin.SkinClass.ControlState.Normal;
+            this.BaseButton_Cancel.DownBack = ((System.Drawing.Image)(resources.GetObject("BaseButton_Cancel.DownBack")));
+            this.BaseButton_Cancel.DrawType = CCWin.SkinControl.DrawStyle.Img;
+            this.BaseButton_Cancel.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.BaseButton_Cancel.Location = new System.Drawing.Point(288, 210);
+            this.BaseButton_Cancel.MouseBack = ((System.Drawing.Image)(resources.GetObject("BaseButton_Cancel.MouseBack")));
+            this.BaseButton_Cancel.Name = "BaseButton_Cancel";
+            this.BaseButton_Cancel.NormlBack = ((System.Drawing.Image)(resources.GetObject("BaseButton_Cancel.NormlBack")));
+            this.BaseButton_Cancel.Size = new System.Drawing.Size(75, 32);
+            this.BaseButton_Cancel.TabIndex = 1;
+            this.BaseButton_Cancel.Text = "取消";
+            this.BaseButton_Cancel.UseVisualStyleBackColor = false;
+            this.BaseButton_Cancel.Click += new System.EventHandler(this.BaseButton_Cancel_Click);
+            // 
+            // BaseButton_OK
+            // 
+            this.BaseButton_OK.BackColor = System.Drawing.Color.Transparent;
+            this.BaseButton_OK.ControlState = CCWin.SkinClass.ControlState.Normal;
+            this.BaseButton_OK.DownBack = ((System.Drawing.Image)(resources.GetObject("BaseButton_OK.DownBack")));
+            this.BaseButton_OK.DrawType = CCWin.SkinControl.DrawStyle.Img;
+            this.BaseButton_OK.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.BaseButton_OK.Location = new System.Drawing.Point(207, 210);
+            this.BaseButton_OK.MouseBack = ((System.Drawing.Image)(resources.GetObject("BaseButton_OK.MouseBack")));
+            this.BaseButton_OK.Name = "BaseButton_OK";
+            this.BaseButton_OK.NormlBack = ((System.Drawing.Image)(resources.GetObject("BaseButton_OK.NormlBack")));
+            this.BaseButton_OK.Size = new System.Drawing.Size(75, 32);
+            this.BaseButton_OK.TabIndex = 0;
+            this.BaseButton_OK.Text = "确定";
+            this.BaseButton_OK.UseVisualStyleBackColor = false;
+            this.BaseButton_OK.Click += new System.EventHandler(this.BaseButton_OK_Click);
+            // 
+            // skinLabel4
+            // 
+            this.skinLabel4.AutoSize = true;
+            this.skinLabel4.BackColor = System.Drawing.Color.Transparent;
+            this.skinLabel4.BorderColor = System.Drawing.Color.White;
+            this.skinLabel4.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.skinLabel4.Location = new System.Drawing.Point(7, 43);
+            this.skinLabel4.Name = "skinLabel4";
+            this.skinLabel4.Size = new System.Drawing.Size(68, 17);
+            this.skinLabel4.TabIndex = 0;
+            this.skinLabel4.Text = "上班开始：";
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Time;
+            this.dateTimePicker1.Location = new System.Drawing.Point(81, 40);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 21);
+            this.dateTimePicker1.TabIndex = 3;
+            this.dateTimePicker1.Value = new System.DateTime(2020, 10, 20, 9, 0, 0, 0);
+            // 
+            // dateTimePicker2
+            // 
+            this.dateTimePicker2.Format = System.Windows.Forms.DateTimePickerFormat.Time;
+            this.dateTimePicker2.Location = new System.Drawing.Point(81, 67);
+            this.dateTimePicker2.Name = "dateTimePicker2";
+            this.dateTimePicker2.Size = new System.Drawing.Size(200, 21);
+            this.dateTimePicker2.TabIndex = 3;
+            this.dateTimePicker2.Value = new System.DateTime(2020, 10, 20, 12, 0, 0, 0);
+            // 
+            // dateTimePicker3
+            // 
+            this.dateTimePicker3.Format = System.Windows.Forms.DateTimePickerFormat.Time;
+            this.dateTimePicker3.Location = new System.Drawing.Point(81, 94);
+            this.dateTimePicker3.Name = "dateTimePicker3";
+            this.dateTimePicker3.Size = new System.Drawing.Size(200, 21);
+            this.dateTimePicker3.TabIndex = 3;
+            this.dateTimePicker3.Value = new System.DateTime(2020, 10, 20, 13, 0, 0, 0);
+            // 
+            // dateTimePicker4
+            // 
+            this.dateTimePicker4.Format = System.Windows.Forms.DateTimePickerFormat.Time;
+            this.dateTimePicker4.Location = new System.Drawing.Point(81, 121);
+            this.dateTimePicker4.Name = "dateTimePicker4";
+            this.dateTimePicker4.Size = new System.Drawing.Size(200, 21);
+            this.dateTimePicker4.TabIndex = 3;
+            this.dateTimePicker4.Value = new System.DateTime(2020, 10, 20, 18, 0, 0, 0);
+            // 
+            // skinLabel1
+            // 
+            this.skinLabel1.AutoSize = true;
+            this.skinLabel1.BackColor = System.Drawing.Color.Transparent;
+            this.skinLabel1.BorderColor = System.Drawing.Color.White;
+            this.skinLabel1.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.skinLabel1.Location = new System.Drawing.Point(7, 70);
+            this.skinLabel1.Name = "skinLabel1";
+            this.skinLabel1.Size = new System.Drawing.Size(68, 17);
+            this.skinLabel1.TabIndex = 0;
+            this.skinLabel1.Text = "上班结束：";
+            // 
+            // skinLabel2
+            // 
+            this.skinLabel2.AutoSize = true;
+            this.skinLabel2.BackColor = System.Drawing.Color.Transparent;
+            this.skinLabel2.BorderColor = System.Drawing.Color.White;
+            this.skinLabel2.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.skinLabel2.Location = new System.Drawing.Point(7, 98);
+            this.skinLabel2.Name = "skinLabel2";
+            this.skinLabel2.Size = new System.Drawing.Size(68, 17);
+            this.skinLabel2.TabIndex = 0;
+            this.skinLabel2.Text = "下午开始：";
+            // 
+            // skinLabel3
+            // 
+            this.skinLabel3.AutoSize = true;
+            this.skinLabel3.BackColor = System.Drawing.Color.Transparent;
+            this.skinLabel3.BorderColor = System.Drawing.Color.White;
+            this.skinLabel3.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.skinLabel3.Location = new System.Drawing.Point(7, 125);
+            this.skinLabel3.Name = "skinLabel3";
+            this.skinLabel3.Size = new System.Drawing.Size(68, 17);
+            this.skinLabel3.TabIndex = 0;
+            this.skinLabel3.Text = "下午结束：";
+            // 
+            // skinLabel5
+            // 
+            this.skinLabel5.AutoSize = true;
+            this.skinLabel5.BackColor = System.Drawing.Color.Transparent;
+            this.skinLabel5.BorderColor = System.Drawing.Color.White;
+            this.skinLabel5.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.skinLabel5.Location = new System.Drawing.Point(31, 151);
+            this.skinLabel5.Name = "skinLabel5";
+            this.skinLabel5.Size = new System.Drawing.Size(44, 17);
+            this.skinLabel5.TabIndex = 0;
+            this.skinLabel5.Text = "迟到：";
+            // 
+            // skinLabel6
+            // 
+            this.skinLabel6.AutoSize = true;
+            this.skinLabel6.BackColor = System.Drawing.Color.Transparent;
+            this.skinLabel6.BorderColor = System.Drawing.Color.White;
+            this.skinLabel6.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.skinLabel6.Location = new System.Drawing.Point(31, 178);
+            this.skinLabel6.Name = "skinLabel6";
+            this.skinLabel6.Size = new System.Drawing.Size(44, 17);
+            this.skinLabel6.TabIndex = 0;
+            this.skinLabel6.Text = "早退：";
+            // 
+            // dateTimePicker5
+            // 
+            this.dateTimePicker5.Format = System.Windows.Forms.DateTimePickerFormat.Time;
+            this.dateTimePicker5.Location = new System.Drawing.Point(81, 147);
+            this.dateTimePicker5.Name = "dateTimePicker5";
+            this.dateTimePicker5.Size = new System.Drawing.Size(95, 21);
+            this.dateTimePicker5.TabIndex = 3;
+            this.dateTimePicker5.Value = new System.DateTime(2020, 10, 20, 9, 1, 0, 0);
+            // 
+            // dateTimePicker6
+            // 
+            this.dateTimePicker6.Format = System.Windows.Forms.DateTimePickerFormat.Time;
+            this.dateTimePicker6.Location = new System.Drawing.Point(187, 147);
+            this.dateTimePicker6.Name = "dateTimePicker6";
+            this.dateTimePicker6.Size = new System.Drawing.Size(95, 21);
+            this.dateTimePicker6.TabIndex = 3;
+            this.dateTimePicker6.Value = new System.DateTime(2020, 10, 20, 10, 0, 0, 0);
+            // 
+            // dateTimePicker7
+            // 
+            this.dateTimePicker7.Format = System.Windows.Forms.DateTimePickerFormat.Time;
+            this.dateTimePicker7.Location = new System.Drawing.Point(81, 174);
+            this.dateTimePicker7.Name = "dateTimePicker7";
+            this.dateTimePicker7.Size = new System.Drawing.Size(95, 21);
+            this.dateTimePicker7.TabIndex = 3;
+            this.dateTimePicker7.Value = new System.DateTime(2020, 10, 20, 17, 0, 0, 0);
+            // 
+            // dateTimePicker8
+            // 
+            this.dateTimePicker8.Format = System.Windows.Forms.DateTimePickerFormat.Time;
+            this.dateTimePicker8.Location = new System.Drawing.Point(187, 174);
+            this.dateTimePicker8.Name = "dateTimePicker8";
+            this.dateTimePicker8.Size = new System.Drawing.Size(95, 21);
+            this.dateTimePicker8.TabIndex = 3;
+            this.dateTimePicker8.Value = new System.DateTime(2020, 10, 20, 18, 0, 0, 0);
+            // 
+            // SettingForm
+            // 
+            this.AcceptButton = this.BaseButton_OK;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(370, 249);
+            this.Controls.Add(this.dateTimePicker8);
+            this.Controls.Add(this.dateTimePicker6);
+            this.Controls.Add(this.dateTimePicker4);
+            this.Controls.Add(this.dateTimePicker2);
+            this.Controls.Add(this.dateTimePicker7);
+            this.Controls.Add(this.dateTimePicker5);
+            this.Controls.Add(this.dateTimePicker3);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.BaseButton_Cancel);
+            this.Controls.Add(this.BaseButton_OK);
+            this.Controls.Add(this.skinLabel6);
+            this.Controls.Add(this.skinLabel3);
+            this.Controls.Add(this.skinLabel5);
+            this.Controls.Add(this.skinLabel2);
+            this.Controls.Add(this.skinLabel1);
+            this.Controls.Add(this.skinLabel4);
+            this.Name = "SettingForm";
+            this.Text = "设置";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private Common.Components.BaseButton BaseButton_Cancel;
+        private Common.Components.BaseButton BaseButton_OK;
+        private CCWin.SkinControl.SkinLabel skinLabel4;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker2;
+        private System.Windows.Forms.DateTimePicker dateTimePicker3;
+        private System.Windows.Forms.DateTimePicker dateTimePicker4;
+        private CCWin.SkinControl.SkinLabel skinLabel1;
+        private CCWin.SkinControl.SkinLabel skinLabel2;
+        private CCWin.SkinControl.SkinLabel skinLabel3;
+        private CCWin.SkinControl.SkinLabel skinLabel5;
+        private CCWin.SkinControl.SkinLabel skinLabel6;
+        private System.Windows.Forms.DateTimePicker dateTimePicker5;
+        private System.Windows.Forms.DateTimePicker dateTimePicker6;
+        private System.Windows.Forms.DateTimePicker dateTimePicker7;
+        private System.Windows.Forms.DateTimePicker dateTimePicker8;
+    }
+}
---
+++ MergeExcel/Forms/SettingForm.cs
@@ -0,0 +1,63 @@
+﻿using CCWin;
+using JGNet.Core.InteractEntity;
+using CJBasic.Loggers;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+
+using System.Text;
+
+using System.Windows.Forms;
+
+namespace JGNet.Common
+{
+    public partial class SettingForm : Common.BaseForm
+    {
+        private SettingsConfiguration configuration = new SettingsConfiguration();
+        private Setting setting = null;
+        public Setting result {
+            get { return setting; }
+            set { setting = value; }
+        }
+
+        public SettingForm(Setting setting = null)
+        {
+            InitializeComponent();
+            this.setting = setting;
+            this.Initialize();
+        }
+
+        private void Initialize()
+        {
+            //this.guideComboBox1.Initialize(GuideComboBoxInitializeType.Null, CommonGlobalCache.CurrentShopID);
+        }
+
+        private void BaseButton_OK_Click(object sender, EventArgs e)
+        {
+            //configuration.Settings = this.setting;
+           // configuration.Save();
+                this.DialogResult = DialogResult.OK;
+        }
+
+        private void BaseButton_Cancel_Click(object sender, EventArgs e)
+        {
+            this.DialogResult = DialogResult.Cancel;
+        }
+         
+    }
+
+
+    public class SettingsConfiguration : CJBasic.Serialization.AgileConfiguration
+    {
+        public List< Setting> Settings { get; set; }
+    }
+
+    [Serializable]
+    public class  Setting
+    {
+        public DateTime Start { get; set; }
+        public DateTime End { get; set; } 
+    }
+}
